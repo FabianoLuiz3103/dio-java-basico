fbdbc30a75f22b63af764a9a06d3cbe72e43d6da
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-20 16:20:01 -0300

    Criando a primeira classe com método main

---
+++ src/br/com/fabianoLuiz3103/anatomiaDasClasses/MinhaClasse.java
@@ -0,0 +1,11 @@
+package br.com.fabianoLuiz3103.anatomiaDasClasses;
+
+public class MinhaClasse {
+
+    //se a classe for executável precisa ter o método main
+    public static void main(String[] args) {
+
+        System.out.println("Imprimindo mensagem do método main da classe Minha Classe");
+    }
+
+}
