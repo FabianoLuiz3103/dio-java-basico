95bc108c7391e4ccfd2f0933958bfac3ceb66559
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-21 16:44:44 -0300

    Documentando classe e método

---
+++ src/br/com/fabianoLuiz3103/documentacao/Calculadora.java
@@ -0,0 +1,32 @@
+package br.com.fabianoLuiz3103.documentacao;
+
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ *     <b>Note:</b> Leia atentamente a documentação desda classe
+ *     para desfrutar dos recursos oferecidos pelo autor.
+ * </p>
+ * @author FabianoLuiz3103
+ * @version 1.0
+ * @since 21/02/2025
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado desde método é a soma de dois números
+     */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm+numeroDois;
+    }
+
+    //comentário em uma linha
+    /*
+    comentário de várias linhas
+     */
+    /**
+     * documentação
+     */
+}
