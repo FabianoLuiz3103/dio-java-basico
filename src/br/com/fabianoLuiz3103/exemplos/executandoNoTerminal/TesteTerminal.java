41216c6109f867ecc0b55069f31d355d9e41520c
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-21 17:43:03 -0300

    Excutando via terminal ms-dos

---
+++ src/br/com/fabianoLuiz3103/exemplos/executandoNoTerminal/TesteTerminal.java
@@ -0,0 +1,9 @@
+package br.com.fabianoLuiz3103.exemplos.executandoNoTerminal;
+
+public class TesteTerminal {
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá! Fui executado pelo terminal!");
+    }
+}
