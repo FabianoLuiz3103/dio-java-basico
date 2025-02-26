e9df1514b675093b111952472e6118f2dd17b0c2
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-26 17:20:26 -0300

    refactor: Implementando os métodos da classe pai ServicoDeMensagem

--- src/br/com/fabianoLuiz3103/oo/servicoMensagem/model/Facebook.java
+++ src/br/com/fabianoLuiz3103/oo/servicoMensagem/model/Facebook.java
@@ -1,4 +1,14 @@
 package br.com.fabianoLuiz3103.oo.servicoMensagem.model;
 
 public class Facebook extends ServicoDeMensagem{
+
+    @Override
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo Facebook");
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo Facebook");
+    }
 }
--- src/br/com/fabianoLuiz3103/oo/servicoMensagem/model/MSNMessenger.java
+++ src/br/com/fabianoLuiz3103/oo/servicoMensagem/model/MSNMessenger.java
@@ -2,4 +2,13 @@ package br.com.fabianoLuiz3103.oo.servicoMensagem.model;
 
 public class MSNMessenger extends ServicoDeMensagem{
 
+    @Override
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo MSN");
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo MSN");
+    }
 }
