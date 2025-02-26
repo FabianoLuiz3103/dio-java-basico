454f4a1b91d0d562c0cce26c2c46414c33bab831
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-26 17:03:51 -0300

    feat: criando classe básica MSN Messenger

---
+++ src/br/com/fabianoLuiz3103/oo/servicoMensagem/model/MSNMessenger.java
@@ -0,0 +1,12 @@
+package br.com.fabianoLuiz3103.oo.servicoMensagem.model;
+
+public class MSNMessenger {
+
+    public void enviarMensagem(){}
+
+    public void receberMensagem(){}
+
+    public void validarConectadoInternet(){}
+
+    public void salvarHistoricoMensagem(){}
+}
