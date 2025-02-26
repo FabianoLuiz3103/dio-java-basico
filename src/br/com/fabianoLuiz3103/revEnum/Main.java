a2e48c28320ae53f9fdf54c9b350d6eb05fca4a4
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-26 16:25:31 -0300

    feat: testando enum

---
+++ src/br/com/fabianoLuiz3103/revEnum/Main.java
@@ -0,0 +1,13 @@
+package br.com.fabianoLuiz3103.revEnum;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EstadoBrasileiro estado = EstadoBrasileiro.SAO_PAULO; //referência estado apotando para o objeto SAO_PAULO
+        System.out.println(estado.getNome() + " - " + estado.getSigla());
+
+        estado = EstadoBrasileiro.BAHIA;//referência estado apontando para o objeto BAHIA
+        System.out.println(estado.getNome() + " - " + estado.getSigla());
+    }
+}
