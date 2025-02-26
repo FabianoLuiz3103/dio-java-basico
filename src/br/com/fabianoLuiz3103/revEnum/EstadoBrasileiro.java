ca402e08f4ef0e42a190906e7541a5ace4ed959d
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-26 16:25:20 -0300

    feat: Criando enum EstadoBrasileiro

---
+++ src/br/com/fabianoLuiz3103/revEnum/EstadoBrasileiro.java
@@ -0,0 +1,27 @@
+package br.com.fabianoLuiz3103.revEnum;
+
+public enum EstadoBrasileiro {
+
+    SAO_PAULO("SP", "São Paulo"),
+    RIO_DE_JANEIRO("RJ", "Rio de Janeiro"),
+    BAHIA("BA", "Bahia"),
+    MINAS_GERAIS("MG", "Minas Gerais")
+    ;
+
+
+    private final String sigla; //final garante a imutabilidade
+    private final String nome;
+
+    EstadoBrasileiro(String sigla, String nome) {
+        this.sigla = sigla;
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
